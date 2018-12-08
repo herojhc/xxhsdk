36a5c2de7da9d6d01d9063e7212f137daab131b8
herojhc <759242210@qq.com>
2018-12-08 09:58:44 +0800

    修改 user 迁移

--- database/migrations/2016_07_23_000000_create_or_update_users_table.php
+++ database/migrations/2016_07_23_000000_create_or_update_users_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class UpdateUsersTable extends Migration
+class CreateOrUpdateUsersTable extends Migration
 {
     /**
      * Run the migrations.
@@ -66,36 +66,88 @@ class UpdateUsersTable extends Migration
             $table->renameColumn('id', 'user_id');
         } else {
             if (!Schema::hasColumn('users', 'user_id')) {
-                $table->increments('user_id');
+                $table->integer('user_id');
+                $table->primary('user_id');
             }
         }
-
         if (Schema::hasColumn('users', 'password')) {
             $table->string('password')->nullable()->default('')->change();
         }
-
-        $email = $table->string('email')->nullable()->default('');
+        $username = $table->string('username', 50)->default('')->nullable()->comment('用户名');
+        if (Schema::hasColumn('users', 'username')) {
+            $username->change();
+        }
+        $job = $table->string('job', 20)->default('')->nullable()->comment('工作');
+        if (Schema::hasColumn('users', 'job')) {
+            $job->change();
+        }
+        $avatar = $table->string('avatar')->default('')->nullable()->comment('头像');
+        if (Schema::hasColumn('users', 'avatar')) {
+            $avatar->change();
+        }
+        $gender = $table->char('gender', 1)->nullable()->comment('性别1男2女0保密');
+        if (Schema::hasColumn('users', 'gender')) {
+            $gender->change();
+        }
+        $birthday = $table->date('birthday')->nullable()->comment('出生日期');
+        if (Schema::hasColumn('users', 'birthday')) {
+            $birthday->change();
+        }
+        $country = $table->char('country', 2)->default('ZH')->nullable()->comment('国家');
+        if (Schema::hasColumn('users', 'country')) {
+            $country->change();
+        }
+        $province = $table->char('province', 6)->default('')->nullable()->comment('省份');
+        if (Schema::hasColumn('users', 'province')) {
+            $province->change();
+        }
+        $city = $table->char('city', 6)->default('')->nullable()->comment('城市');
+        if (Schema::hasColumn('users', 'city')) {
+            $city->change();
+        }
+        $timezone = $table->string('timezone', 30)->default('PRC')->nullable()->comment('时区');
+        if (Schema::hasColumn('users', 'timezone')) {
+            $timezone->change();
+        }
+        $locale = $table->string('locale', 15)->default('zh')->nullable()->comment('地区');
+        if (Schema::hasColumn('users', 'locale')) {
+            $locale->change();
+        }
+        $mobile = $table->string('mobile', 11)->nullable()->default('')->comment('手机');
+        if (Schema::hasColumn('users', 'mobile')) {
+            $mobile->change();
+        }
+        $mobile_validated = $table->boolean('mobile_validated')->nullable()->default(0)->comment('手机认证');
+        if (Schema::hasColumn('users', 'mobile_validated')) {
+            $mobile_validated->change();
+        }
+        $email = $table->string('email')->nullable()->default('')->comment('邮箱');
         if (Schema::hasColumn('users', 'email')) {
             $email->change();
         }
-
-        $table->string('username', 50)->default('')->nullable();
-        $table->string('job', 20)->default('')->nullable();
-        $table->string('avatar', 191)->default('');
-        $table->char('gender', 1)->nullable();
-        $table->date('birthday')->nullable();
-        $table->char('country', 2)->default('ZH')->nullable();
-        $table->char('province', 6)->default('')->nullable();
-        $table->char('city', 6)->default('')->nullable();
-        $table->string('timezone', 30)->default('PRC')->nullable();
-        $table->string('locale', 15)->default('zh')->nullable();
-        $table->string('mobile', 11)->nullable()->default('');
-        $table->boolean('mobile_validated')->nullable()->default(0);
-        $table->boolean('email_validated')->nullable()->default(0);
-        $table->timestamp('email_validated_at')->nullable();
-        $table->boolean('name_validated')->nullable()->default(0);
-        $table->boolean('status')->default(0);
-        $table->string('api_token', 100)->default('');
-        $table->smallInteger('source')->default(0)->nullable();
+        $email_validated = $table->boolean('email_validated')->nullable()->default(0)->comment('邮箱认证');
+        if (Schema::hasColumn('users', 'email_validated')) {
+            $email_validated->change();
+        }
+        $email_validated_at = $table->timestamp('email_validated_at')->nullable()->comment('邮箱认证时间');
+        if (Schema::hasColumn('users', 'email_validated_at')) {
+            $email_validated_at->change();
+        }
+        $name_validated_at = $table->boolean('name_validated')->nullable()->default(0)->comment('实名认证');
+        if (Schema::hasColumn('users', 'name_validated')) {
+            $name_validated_at->change();
+        }
+        $status = $table->boolean('status')->default(0)->comment('状态1激活0未激活-1已废弃');
+        if (Schema::hasColumn('users', 'status')) {
+            $status->change();
+        }
+        $api_token = $table->string('api_token', 100)->default('')->nullable()->comment('API_TOKEN');
+        if (Schema::hasColumn('users', 'api_token')) {
+            $api_token->change();
+        }
+        $source = $table->smallInteger('source')->default(0)->nullable()->comment('来源');
+        if (Schema::hasColumn('users', 'source')) {
+            $source->change();
+        }
     }
 }
