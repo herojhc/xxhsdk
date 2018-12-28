e222f8e5c3091be374fff703788012ff25cc557f
herojhc <759242210@qq.com>
2018-12-28 11:56:09 +0800

    增加 文件服务接口

---
+++ src/XinXiHua/SDK/Services/FileService.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: JHC
+ * Date: 2018-12-28
+ * Time: 11:53
+ */
+
+namespace XinXiHua\SDK\Services;
+
+
+use XinXiHua\SDK\Exceptions\ApiException;
+
+class FileService extends BaseService
+{
+
+    public function upload($file, $type, $name = '')
+    {
+
+        // 调用存储API
+        $url = 'files';
+        if (!empty($type)) {
+            $url .= '?type=' . $type;
+        }
+
+        $response = $this->client->postMultipartSimple($url, [
+            'name' => $name,
+            'attachment' => $file
+        ]);
+
+
+        if ($response->isResponseSuccess()) {
+            return $response->getResponseData()['data'];
+        }
+
+        throw new ApiException($response->getResponseData()['message']);
+    }
+}
