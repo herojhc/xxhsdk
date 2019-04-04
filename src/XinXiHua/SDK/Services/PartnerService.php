2462d88f914a1c7909be075848772534d9866ae1
herojhc <759242210@qq.com>
2019-04-04 16:43:48 +0800

    新增伙伴和 平台签约 接口

---
+++ src/XinXiHua/SDK/Services/PartnerService.php
@@ -0,0 +1,64 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: JHC
+ * Date: 2019-04-04
+ * Time: 12:09
+ */
+
+namespace XinXiHua\SDK\Services;
+
+
+use XinXiHua\SDK\Exceptions\ApiException;
+
+class PartnerService extends BaseService
+{
+
+    /**
+     * @param $partner
+     * @param string $type
+     * @param null $corpId
+     * @return mixed
+     * @throws ApiException
+     */
+    public function sendVerifyCode($partner, $type = 'code', $corpId = null)
+    {
+        $response = $this->accessToken->getIsvCorpClient($corpId)->post('/partners/' . $partner . '/verify-code?type=' . $type);
+        if ($response->isResponseSuccess()) {
+            return $response->getResponseData()['data'];
+        }
+        throw new ApiException($response->getResponseMessage());
+
+    }
+
+    /**
+     * @param $partnerId
+     * @param $data
+     * @param null $corpId
+     * @return bool
+     * @throws ApiException
+     */
+    public function sign($partnerId, $data, $corpId = null)
+    {
+        $response = $this->accessToken->getIsvCorpClient($corpId)->post('/partners/' . $partnerId . '/sign', $data);
+        if ($response->isResponseSuccess()) {
+            return true;
+        }
+        throw new ApiException($response->getResponseMessage());
+    }
+
+    /**
+     * @param $partnerId
+     * @param null $corpId
+     * @return bool
+     * @throws ApiException
+     */
+    public function release($partnerId, $corpId = null)
+    {
+        $response = $this->accessToken->getIsvCorpClient($corpId)->post('/partners/' . $partnerId . '/release');
+        if ($response->isResponseSuccess()) {
+            return true;
+        }
+        throw new ApiException($response->getResponseMessage());
+    }
+}
---
+++ src/XinXiHua/SDK/Services/PlatformService.php
@@ -0,0 +1,48 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: JHC
+ * Date: 2019-04-04
+ * Time: 12:09
+ */
+
+namespace XinXiHua\SDK\Services;
+
+
+use XinXiHua\SDK\Exceptions\ApiException;
+
+class PlatformService extends BaseService
+{
+
+    /**
+     * @param $platform
+     * @param string $type
+     * @param null $corpId
+     * @return mixed
+     * @throws ApiException
+     */
+    public function apply($platform, $type = 'code', $corpId = null)
+    {
+        $response = $this->accessToken->getIsvCorpClient($corpId)->post('/platforms/' . $platform . '/apply?type=' . $type);
+        if ($response->isResponseSuccess()) {
+            return $response->getResponseData()['data']['id'];
+        }
+        throw new ApiException($response->getResponseMessage());
+
+    }
+
+    /**
+     * @param $platformId
+     * @param null $corpId
+     * @return bool
+     * @throws ApiException
+     */
+    public function release($platformId, $corpId = null)
+    {
+        $response = $this->accessToken->getIsvCorpClient($corpId)->post('/platforms/' . $platformId . '/release');
+        if ($response->isResponseSuccess()) {
+            return true;
+        }
+        throw new ApiException($response->getResponseMessage());
+    }
+}
