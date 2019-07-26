b601244f9116c92fa822c2df638e774fb54ebdae
herojhc <759242210@qq.com>
2019-07-26 15:17:11 +0800

    Isv install fix

--- src/XinXiHua/SDK/Services/IsvService.php
+++ src/XinXiHua/SDK/Services/IsvService.php
@@ -22,20 +22,12 @@ class IsvService
 {
     public function install($eventMsg)
     {
-
-        $corpId = $eventMsg->CorpId;
-        $corpName = $eventMsg->CorpName;
-        $permanentCode = $eventMsg->PermanentCode;
-        $authUser = $eventMsg->AuthUser;
-
         try {
             Log::debug('installingLog', [
-                'log' =>
-                    [
-                        $corpId,
-                        $permanentCode,
-                        $authUser
-                    ]
+                'log' => [
+                    'corpId' => $eventMsg->CorpId,
+                    'permanentCode' => $eventMsg->PermanentCode
+                ]
             ]);
         } catch (\Throwable $exception) {
 
@@ -43,76 +35,76 @@ class IsvService
 
         try {
             //
-            DB::beginTransaction();
-            $authInfo = CorporationPermanentCode::query()->where([
-                ['corp_id', $corpId],
-                ['agent_id', config('xxh-sdk.agent.agent_id')]
-            ])->first();
+            DB::transaction(function () use ($eventMsg) {
+                $corpId = $eventMsg->CorpId;
+                $corpName = $eventMsg->CorpName;
+                $permanentCode = $eventMsg->PermanentCode;
+                $authUser = $eventMsg->AuthUser;
+                $authInfo = CorporationPermanentCode::query()->where([
+                    ['corp_id', $corpId],
+                    ['agent_id', config('xxh-sdk.agent.agent_id')]
+                ])->first();
 
-            if ($authInfo) {
+                if ($authInfo) {
 
-                $authInfo->permanent_code = $permanentCode;
-                $authInfo->saveOrFail();
+                    $authInfo->permanent_code = $permanentCode;
+                    $authInfo->saveOrFail();
+                    return 'success';
+                }
 
-                return 'success';
-            }
 
+                $arr['permanent_code'] = $permanentCode;
+                $arr['corp_id'] = $corpId;
+                $arr['agent_id'] = config('xxh-sdk.agent.agent_id');
+                $arr['name'] = $corpName;
 
-            $arr['permanent_code'] = $permanentCode;
-            $arr['corp_id'] = $corpId;
-            $arr['agent_id'] = config('xxh-sdk.agent.agent_id');
-            $arr['name'] = $corpName;
+                if (CorporationPermanentCode::forceCreate($arr)) {
 
-            if (CorporationPermanentCode::forceCreate($arr)) {
+                    // 添加一条记录到 corporations表
+                    Corporation::query()->updateOrCreate([
+                        'corp_id' => $corpId
+                    ],
+                        [
+                            'name' => $corpName,
+                            'status' => 1,
+                            'user_id' => $authUser->user_id
+                        ]);
 
-                // 添加一条记录到 corporations表
-                Corporation::query()->updateOrCreate([
-                    'corp_id' => $corpId
-                ],
-                    [
-                        'name' => $corpName,
-                        'status' => 1,
+                    // 添加管理员记录
+                    User::query()->updateOrCreate([
                         'user_id' => $authUser->user_id
+                    ], [
+                        'name' => $authUser->name,
+                        'mobile' => $authUser->mobile,
+                        'avatar' => $authUser->avatar,
+                        'mobile_validated' => 1
                     ]);
 
-                // 添加管理员记录
-                User::query()->updateOrCreate([
-                    'user_id' => $authUser->user_id
-                ], [
-                    'name' => $authUser->name,
-                    'mobile' => $authUser->mobile,
-                    'avatar' => $authUser->avatar,
-                    'mobile_validated' => 1
-                ]);
+                    // 添加联系人
+                    Contact::query()->updateOrCreate([
+                        'contact_id' => $authUser->contact_id
+                    ], [
+                        'code' => $authUser->code,
+                        'name' => $authUser->name,
+                        'mobile' => $authUser->mobile,
+                        'avatar' => $authUser->avatar,
+                        'corp_id' => $corpId,
+                        'user_id' => $authUser->user_id,
+                        'is_admin' => 1
+                    ]);
 
-                // 添加联系人
-                Contact::query()->updateOrCreate([
-                    'contact_id' => $authUser->contact_id
-                ], [
-                    'code' => $authUser->code,
-                    'name' => $authUser->name,
-                    'mobile' => $authUser->mobile,
-                    'avatar' => $authUser->avatar,
-                    'corp_id' => $corpId,
-                    'user_id' => $authUser->user_id,
-                    'is_admin' => 1
-                ]);
+                    $corp = Corporation::find($corpId);
+                    $contact = Contact::find($authUser->contact_id);
+                    $user = User::find($authUser->user_id);
+                    // 触发事件
+                    event(new Installed($corp, $user, $contact));
+                    // 一定要返回success
+                    return 'success';
+                }
+            });
 
-
-                $corp = Corporation::find($corpId);
-                $contact = Contact::find($authUser->contact_id);
-                $user = User::find($authUser->user_id);
-                // 触发事件
-                event(new Installed($corp, $user, $contact));
-
-                DB::commit();
-                // 一定要返回success
-                return 'success';
-
-            }
         } catch (\Throwable $throwable) {
             Log::error($throwable->getMessage(), ['exception' => $throwable]);
-            DB::rollBack();
         }
 
         return 'error';
